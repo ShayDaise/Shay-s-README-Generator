41629bb5d22846e59705ef4c31b39b02738a264f
ShayDaise <102685776+ShayDaise@users.noreply.github.com>
2022-07-07 18:46:54 -0400

    added markdown logic for the readme generator function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,53 @@
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  ## Project Title
+  ${data.projectTitle}
+  
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+  
+  
+  ## Description
+    ${data.description}
+  
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  
+  ## Installation
+   ${data.installation}
+  
+  
+  ## Usage
+   ${data.usage}
+  
+  
+  ## License
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+  
+  This application is covered by the ${data.license} license. 
+  
+  ## Contributing
+   ${data.contributing}
+  
+  ## Tests
+   ${data.tests}
+  
+  ## Questions
+   ${data.questions}
+  
+  Find me on GitHub: [${data.username}](https://github.com/${data.username})
+  
+  Email me with any questions: ${data.email}
+      `;
+    }
 
-`;
-}
 
 module.exports = generateMarkdown;

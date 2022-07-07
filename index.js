c68110edfdd59d007a566e9869829ec2d4cf6ded
ShayDaise <102685776+ShayDaise@users.noreply.github.com>
2022-07-07 09:38:11 -0400

    added initial files from repo

---
+++ index.js
@@ -0,0 +1,21 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+
+// need inquirer variable here
+const inquirer = require("inquirer");
+
+// need a markdown js file here
+const generateMarkdown = require("./utils/generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = [
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

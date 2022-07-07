89456fc3a52c334c9f602c53136d60b52cd18c46
ShayDaise <102685776+ShayDaise@users.noreply.github.com>
2022-07-07 09:38:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const inquirer = require("inquirer");
 
 // need a markdown js file here
 const generateMarkdown = require("./utils/generateMarkdown");
+
+// created validate function for the questions to make sure they get answered
+function validateInput(value) {
+    if (value != "") {
+        return true;
+    } else {
+        return "Please answer the question with some kind on input.";
+    }
+}
+
 // TODO: Create an array of questions for user input
 const questions = [
     

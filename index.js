bdb908686b2609286be833e8277331827ceb9d2b
ShayDaise <102685776+ShayDaise@users.noreply.github.com>
2022-07-07 09:55:24 -0400

    created all the questions for inquirer to ask, deleted some of the markdown functions

--- index.js
+++ index.js
@@ -18,14 +18,85 @@ function validateInput(value) {
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
+
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the project title?",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: ",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process if any: ",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is this project usage for?",
+        validate: validateInput,
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose the appropriate license for this project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ],
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this projects?",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Is there a test included?",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue? ",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username: ",
+        validate: validateInput,
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email: ",
+        validate: validateInput,
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
